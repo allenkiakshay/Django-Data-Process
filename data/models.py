e0b5950c2325511301d88d6f81307362aca6e8d8
Allenki Akshay <111362177+allenkiakshay@users.noreply.github.com>
2023-04-10 23:00:34 +0530

    Update models.py

--- data/models.py
+++ data/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Input(models.Model):
     image = models.FileField(upload_to='images/')
     text = models.CharField(max_length=5000)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
